2f3394108cad6c98c8feeaf4b8d608cbc5d3ae04
arildrotmo <arotmo@gmail.com>
2014-03-14 22:53:48 +0100

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var gulp = require('gulp');
+
+// Load plugins
+var $ = require('gulp-load-plugins')();
+
+
+// Styles
+gulp.task('styles', function () {
+    return gulp.src('styles.scss')
+        .pipe($.rubySass({
+          style: 'expanded'
+        }))
+        .pipe($.autoprefixer('last 1 version'))
+        .pipe(gulp.dest('_css'))
+});
+
+
+// Default task
+gulp.task('default', function () {
+    gulp.start('styles');
+});
+
+// Watch
+gulp.task('watch', function () {
+    // Watch .scss files
+    gulp.watch('**/*.scss', ['styles']);
+});
